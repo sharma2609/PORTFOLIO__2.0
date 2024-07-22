b9691cbfb637f9a5c3d258c6e8aef2fb44533fdc
Priyanshu Sharma <priyanshu.sharma.cse.2020@miet.ac.in>
2024-07-23 01:16:43 +0530

    cursor improved

--- src/Components/cursor/Cursor.jsx
+++ src/Components/cursor/Cursor.jsx
@@ -4,23 +4,45 @@ import "./cursor.scss";
 
 const Cursor = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [hovered, setHovered] = useState(false);
 
   useEffect(() => {
     const mouseMove = (e) => {
       setPosition({ x: e.clientX, y: e.clientY });
     };
 
+    const mouseEnter = () => {
+      setHovered(true);
+    };
+
+    const mouseLeave = () => {
+      setHovered(false);
+    };
+
     window.addEventListener("mousemove", mouseMove);
+    document.querySelectorAll("a").forEach(link => {
+      link.addEventListener("mouseenter", mouseEnter);
+      link.addEventListener("mouseleave", mouseLeave);
+    });
 
     return () => {
       window.removeEventListener("mousemove", mouseMove);
+      document.querySelectorAll("a").forEach(link => {
+        link.removeEventListener("mouseenter", mouseEnter);
+        link.removeEventListener("mouseleave", mouseLeave);
+      });
     };
   }, []);
 
   return (
     <motion.div
       className="cursor"
-      animate={{ x: position.x+10, y: position.y+10 }}
+      animate={{
+        x: position.x - 25,
+        y: position.y - 25,
+        width: hovered ? 50 : 20,
+        height: hovered ? 50 : 20
+      }}
     ></motion.div>
   );
 };
